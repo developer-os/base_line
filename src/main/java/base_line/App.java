0cb5569abfb73155234c40146307a0efa37d7977
rchen7 <rocky.chen@morningstar.com>
2019-10-04 13:37:58 +0800

    c

--- src/main/java/base_line/App.java
+++ src/main/java/base_line/App.java
@@ -1,5 +1,7 @@
 package base_line;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class);
     }
 }
